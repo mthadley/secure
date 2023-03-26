c91cd83f0ebc0c1301c687f3f8ce13c61bdebc92
Michael Hadley <m@mthadley.com>
2023-03-25 18:01:33 -0700

    Add language servers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+	let count = 0;
+
+	count.toString();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class:danger={count > 5}>{count}</div>
+
+<button on:click={() => count++}>increment</button>
+
+<style>
+	.danger {
+		color: red;
+	}
+</style>
